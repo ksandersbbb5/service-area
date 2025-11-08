c8261104a916cda019cb7b473a074cfb469aaad6
ksandersbbb5 <ksanders@boston.bbb.org>
2025-11-07 23:07:09 -0500

    Refactor App component to include header and logo

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,25 @@ import BBBServiceAreaMap from "./BBBServiceAreaMap";
 export default function App() {
   return (
     <div>
-      <h1 style={{textAlign: "center"}}>BBB New England Service Area Map</h1>
-      <BBBServiceAreaMap />
+      <header
+        style={{
+          display: "flex",
+          alignItems: "center",
+          padding: "10px 20px",
+          borderBottom: "1px solid #ddd",
+        }}
+      >
+        <img
+          src="/bluechatlogo.png"
+          alt="BBB Logo"
+          style={{ height: "50px", marginRight: "15px" }}
+        />
+        <h1 style={{ margin: 0 }}>BBB New England Service Area Map</h1>
+      </header>
+
+      <main style={{ textAlign: "center", marginTop: "20px" }}>
+        <BBBServiceAreaMap />
+      </main>
     </div>
   );
 }
