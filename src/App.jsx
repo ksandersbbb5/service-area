c99003b9ca3fa3b1485dab71be5031ddbb3aa124
ksandersbbb5 <ksanders@boston.bbb.org>
2025-11-07 23:13:50 -0500

    Refactor App component to use inline styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
 import React from "react";
 import BBBServiceAreaMap from "./BBBServiceAreaMap";
-import "./index.css"; // make sure this is imported so Verdana applies globally
 
 export default function App() {
+  const styles = {
+    app: {
+      fontFamily: "Verdana, Geneva, sans-serif",
+    },
+    header: {
+      position: "relative",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      padding: "10px 20px",
+      borderBottom: "1px solid #ddd",
+    },
+    logo: {
+      position: "absolute",
+      left: 20,
+      top: 10,
+      height: 50,
+    },
+    title: {
+      margin: 0,
+      fontSize: "1.5rem",
+      textAlign: "center",
+    },
+    main: {
+      textAlign: "center",
+      marginTop: 20,
+    },
+  };
+
   return (
-    <div className="app-container">
-      <header className="app-header">
-        <img
-          src="/bluechatlogo.png"
-          alt="BBB Logo"
-          className="bbb-logo"
-        />
-        <h1 className="app-title">
-          BBB in E. MA, ME, RI & VT Service Area Map
-        </h1>
+    <div style={styles.app}>
+      <header style={styles.header}>
+        {/* NOTE: file must be in /public as bluechatlogo.png */}
+        <img src="/bluechatlogo.png" alt="BBB Logo" style={styles.logo} />
+        <h1 style={styles.title}>BBB in E. MA, ME, RI & VT Service Area Map</h1>
       </header>
 
-      <main className="app-main">
+      <main style={styles.main}>
         <BBBServiceAreaMap />
       </main>
     </div>
