493045c4c3f35f85475cf011db550362396cc0fe
ksandersbbb5 <ksanders@boston.bbb.org>
2025-11-07 19:58:40 -0500

    Add App component with service area map

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import BBBServiceAreaMap from "./BBBServiceAreaMap";
+
+export default function App() {
+  return (
+    <div>
+      <h1 style={{textAlign: "center"}}>BBB New England Service Area Map</h1>
+      <BBBServiceAreaMap />
+    </div>
+  );
+}
